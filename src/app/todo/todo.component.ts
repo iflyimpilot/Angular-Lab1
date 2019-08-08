b3521e4467f781d46dae04995b5171059b9090a8
iflyimpilot <iflyimpilot@gmail.com>
2019-08-08 00:08:28 -0400

    first commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { todo } from "../interface/todo";
+
+@Component({
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"]
+})
+export class TodoComponent implements OnInit {
+  toDos: list[] = [
+    { task: "Cook dinner", completed: true },
+    { task: "Wash dishes", completed: true },
+    { task: "Buy groceries", completed: false },
+    { task: "Clean up", completed: true },
+    { task: "Watch tv", completed: false },
+    { task: "Reading", completed: true },
+    { task: "Sleep", completed: true }
+  ];
+
+  constructor() {}
+
+  removeItem(index: number): void {
+    this.toDos.splice(index, 1);
+  }
+
+  ngOnInit() {}
+}
